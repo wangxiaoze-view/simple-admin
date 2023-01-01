f09314ad969117bc01dafdb4270fbb53e1b6d876
wangxiaoze <wangxiaoze@petalmail.com>
2023-01-01 17:49:18 +0800

    chore: 配置化信息完善

---
+++ src/config/theme.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // 布局方式  普通布局ordinary  横向布局row  分拦布局column
+  layout: 'ordinary',
+  // 主题颜色
+  themeName: '',
+  // 头部是否固定
+  isHeaderFixed: true,
+  // 标签页是否展示
+  isTabs: true,
+  // 菜单的缩放
+  collapse: false,
+};
