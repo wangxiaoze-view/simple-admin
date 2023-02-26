ec76179c63f76d328ada98777da5d8b7d5b6aa3d
wangxiaoze <wangxiaoze@petalmail.com>
2023-02-26 16:13:58 +0800

    chore: 文档更新

--- src/views/home/analyse/index.vue
+++ src/views/home/analyse/index.vue
@@ -1,10 +1,17 @@
 <template>
-  <div className="analyse-main-s-container">分析台</div>
+  <div className="analyse-main-s-container">
+    <sim-bench-card />
+  </div>
 </template>
 
 <script>
+  import SimBenchCard from './components/benchCard.vue'
+
   export default {
     name: 'Analyse',
+    components: {
+      SimBenchCard,
+    },
   }
 </script>
 
